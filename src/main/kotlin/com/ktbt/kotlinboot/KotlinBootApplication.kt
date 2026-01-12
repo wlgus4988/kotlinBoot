be842da175987ecc6a3c8111e62f2553352b4582
wlgus4988 <wlgus4988@weaverloft.com>
2026-01-12 10:50:40 +0900

    첫번째 커밋

---
+++ src/main/kotlin/com/ktbt/kotlinboot/KotlinBootApplication.kt
@@ -0,0 +1,11 @@
+package com.ktbt.kotlinboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinBootApplication>(*args)
+}
---
+++ src/test/kotlin/com/ktbt/kotlinboot/KotlinBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ktbt.kotlinboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
